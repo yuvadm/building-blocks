d71a2bc0cf6a14523d51828d5d1ad47075bbdb3f
Yuval Adam <yuv.adm@gmail.com>
2011-04-01 12:36:30 +0300

    added s3 stuff

---
+++ s3/gen_policy.py
@@ -0,0 +1,21 @@
+import base64
+import hmac, hashlib
+
+POLICY = """{"expiration": "2012-01-01T00:00:00Z",
+  "conditions": [
+    {"bucket": "AWS_BUCKET_NAME"}, 
+    ["starts-with", "$key", "uploads/"],
+    {"acl": "private"},
+    {"success_action_redirect": "/"},
+    ["starts-with", "$Content-Type", ""],
+    ["content-length-range", 0, 1048576]
+  ]
+}"""
+
+KEY = "AWS_SECRET_KEY"
+
+policy = base64.b64encode(POLICY)
+
+sig = base64.b64encode(hmac.new(KEY, policy, hashlib.sha1).digest())
+
+print sig
